e7c1329be27e84d6cbfce94ed61abee4ea8c77e6
cuongnt02 <cuong.ntc02@gmail.com>
2024-07-19 15:31:40 +0700

    Task: Title Detail - Add clapter list and chapter info

--- app/src/main/java/com/main/comicapp/activities/TitleDetailActivity.java
+++ app/src/main/java/com/main/comicapp/activities/TitleDetailActivity.java
@@ -5,12 +5,20 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.main.comicapp.R;
+import com.main.comicapp.adapters.ChaptersAdapter;
 import com.main.comicapp.enums.PubStatus;
+import com.main.comicapp.models.Chapter;
 import com.main.comicapp.models.Genre;
 import com.main.comicapp.models.Title;
+import com.main.comicapp.viewmodels.ChapterViewModel;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class TitleDetailActivity extends AppCompatActivity {
@@ -20,17 +28,39 @@ public class TitleDetailActivity extends AppCompatActivity {
     private TextView txtViews;
     private TextView txtCreatedDate;
     private TextView txtPublishStatus;
+    private RecyclerView rvChapters;
+    private ChaptersAdapter chaptersAdapter;
+    private ChapterViewModel chapterViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title_detail);
 
+        // Initialize views
         txtTitleName = findViewById(R.id.title_detail_title_name);
         txtGenres = findViewById(R.id.title_detail_genres);
         txtViews = findViewById(R.id.title_detail_views);
         txtCreatedDate = findViewById(R.id.title_detail_created_date);
         txtPublishStatus = findViewById(R.id.title_detail_publishing_status);
+        rvChapters = findViewById(R.id.title_detail_chapter_list);
+
+        // Initialize RecyclerView
+        initRv();
+
+    }
+
+    private void initRv() {
+        rvChapters.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        chaptersAdapter = new ChaptersAdapter(this, null);
+        chaptersAdapter.setListener(new ChaptersAdapter.OnChapterClickListener() {
+            @Override
+            public void onChapterClick(Chapter chapter) {
+                intentToReading(chapter);
+            }
+        });
+        rvChapters.setAdapter(chaptersAdapter);
+        chapterViewModel = new ViewModelProvider(this).get(ChapterViewModel.class);
     }
 
     @Override
@@ -40,6 +70,14 @@ public class TitleDetailActivity extends AppCompatActivity {
         Title title = (Title) intent.getSerializableExtra("title");
         if (title != null) {
             loadTitleData(title);
+            chapterViewModel.getChapters(title.getId()).observeForever(new Observer<List<Chapter>>() {
+                @Override
+                public void onChanged(List<Chapter> chapters) {
+                    if (chapters != null) {
+                        chaptersAdapter.setChapters(chapters);
+                    }
+                }
+            });
         }
     }
 
@@ -50,5 +88,11 @@ public class TitleDetailActivity extends AppCompatActivity {
         txtViews.setText(String.valueOf(title.getViews()));
         txtCreatedDate.setText(title.getUploadedDate().toString());
         txtPublishStatus.setText(PubStatus.valueOf(title.getPubStatus()).toString());
+    }
+
+    private void intentToReading(Chapter chapter) {
+        Intent intent = new Intent(getApplicationContext(), ReadingActivity.class);
+        intent.putExtra("chapter", chapter);
+        startActivity(intent);
     }
 }
