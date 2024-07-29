678e0c8a4e7b1197aa7c18caf05453e626063cc1
chinhtran07 <chinh.t1807@gmail.com>
2024-07-29 23:27:21 +0700

    feat: add admin title management ui

--- app/src/main/java/com/main/comicapp/fragments/title/TitleFragment.java
+++ app/src/main/java/com/main/comicapp/fragments/title/TitleFragment.java
@@ -4,62 +4,97 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.main.comicapp.R;
+import com.main.comicapp.adapters.TitleAdapter;
+import com.main.comicapp.databinding.FragmentTitleManagementBinding;
+import com.main.comicapp.models.Title;
+import com.main.comicapp.viewmodels.TitleViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TitleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class TitleFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class TitleFragment extends Fragment{
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TitleFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TitleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TitleFragment newInstance(String param1, String param2) {
-        TitleFragment fragment = new TitleFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private RecyclerView recyclerView;
+    private TitleAdapter titleAdapter;
+    private TitleViewModel titleViewModel;
+    private FragmentTitleManagementBinding binding;
+    private NavController navController;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title_management, container, false);
+        binding = FragmentTitleManagementBinding.inflate(inflater, container, false);
+        View root =  binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        titleViewModel = new TitleViewModel();
+        navController = Navigation.findNavController(view);
+
+        recyclerView = binding.recyclerComics;
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        titleAdapter = new TitleAdapter(getContext(), new ArrayList<>());
+
+        titleViewModel.getTitles(new HashMap<>()).observe(getViewLifecycleOwner(), new Observer<List<Title>>() {
+            @Override
+            public void onChanged(List<Title> titles) {
+                if (titles != null) {
+                    titleAdapter.setTitles(titles);
+                }
+            }
+        });
+
+        titleAdapter.setListener(new TitleAdapter.OnTitleClickListener() {
+            @Override
+            public void onTitleClick(Title title) {
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("title", title);
+                NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_admin);
+                navController.navigate(R.id.nav_comic_detail, bundle);
+            }
+        });
+
+        recyclerView.setAdapter(titleAdapter);
+
+
+        SearchView searchView = binding.searchComic;
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Implement search logic
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Implement search logic
+                return false;
+            }
+        });
+
+        FloatingActionButton fabAddComic = binding.fabAddComic;
+        fabAddComic.setOnClickListener(v -> {
+            navController.navigate(R.id.nav_add_comic);
+        });
     }
 }
