22e2432372a2415bab17f2ecbc2da6c8bd0e611d
nqhQuocHung <hungmt2426@gmail.com>
2024-09-08 01:28:52 +0700

    update chat

--- app/src/main/java/com/main/comicapp/repositories/impl/ChatRoomRepositoryImpl.java
+++ app/src/main/java/com/main/comicapp/repositories/impl/ChatRoomRepositoryImpl.java
@@ -1,18 +1,14 @@
 package com.main.comicapp.repositories.impl;
 
+import androidx.lifecycle.LiveData;
+
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.main.comicapp.models.ChatRoom;
 import com.main.comicapp.repositories.ChatRoomRepository;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.main.comicapp.repositories.DataCallback;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ChatRoomRepositoryImpl implements ChatRoomRepository {
 
@@ -33,51 +29,11 @@ public class ChatRoomRepositoryImpl implements ChatRoomRepository {
         chatRoom.setRoomId(roomId);
         return db.child(roomId).setValue(chatRoom);
     }
-
     @Override
-    public void getChatRoomsForUser(String userId, DataCallback<List<ChatRoom>> callback) {
-        Query senderQuery = db.orderByChild("senderId").equalTo(userId);
-        Query receiverQuery = db.orderByChild("receiverId").equalTo(userId);
-
-        List<ChatRoom> chatRooms = new ArrayList<>();
-
-        // Lấy chat room nơi user là người gửi
-        senderQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot senderSnapshot) {
-                for (DataSnapshot snapshot : senderSnapshot.getChildren()) {
-                    ChatRoom chatRoom = snapshot.getValue(ChatRoom.class);
-                    if (chatRoom != null) {
-                        chatRooms.add(chatRoom);
-                    }
-                }
-
-                // Sau khi lấy xong, lấy tiếp chat room nơi user là người nhận
-                receiverQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot receiverSnapshot) {
-                        for (DataSnapshot snapshot : receiverSnapshot.getChildren()) {
-                            ChatRoom chatRoom = snapshot.getValue(ChatRoom.class);
-                            if (chatRoom != null && !chatRooms.contains(chatRoom)) {
-                                chatRooms.add(chatRoom);
-                            }
-                        }
-
-                        // Gọi callback với danh sách các chat rooms
-                        callback.onSuccess(chatRooms);
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        callback.onFailure(databaseError.toException());
-                    }
-                });
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                callback.onFailure(databaseError.toException());
-            }
-        });
+    public Query getChatRoomsForUser(String userId) {
+        return db.orderByChild("senderId").equalTo(userId)
+                .getRef().orderByChild("receiverId").equalTo(userId);
     }
+
+
 }
